2eb5f15fc5757ae08412f477e54fb8e73709d9d0
MaraAbbracchio80 <108152603+MaraAbbracchio80@users.noreply.github.com>
2026-06-11 15:51:54 +0200

    Add Firebase dashboard config

---
+++ dashboard/firebase-config.js
@@ -0,0 +1,18 @@
+window.MARASENSE_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyC9ixzft776h8539TUf4oKt4KBg9L6zwAU",
+  authDomain: "marasenseexperiences-ar.firebaseapp.com",
+  projectId: "marasenseexperiences-ar",
+  storageBucket: "marasenseexperiences-ar.firebasestorage.app",
+  messagingSenderId: "18861350761",
+  appId: "1:18861350761:web:bb8e605d17a82a8fa9e7f8"
+};
+
+window.MARASENSE_ACCESS = {
+  superAdmins: [
+    "m.abbracchio@newdigitalapp.com"
+  ],
+  managers: [
+    "a.celotto@newdigitalapp.com"
+  ],
+  clientAnalytics: []
+};
